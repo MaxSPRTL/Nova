15ae7e1369e926c5e67375a39d886185afa644a9
MaxSPRTL <MaxSPRTL>
2021-07-20 11:14:10 +0200

    MovePlayer: add jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed;
+    public float jumpForce = 250f;
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
@@ -15,9 +21,25 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer(horizontalMovement);
     }
 
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+
     private void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
     }
 }
